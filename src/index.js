04f9de203c059a7f11fde3d3f37c677f6027fd90
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-04-12 12:23:05 -0400

    User Input Event
    Added a function to take user input and put it in state

--- src/index.js
+++ src/index.js
@@ -3,14 +3,23 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class Example extends React.Component {
+  state = {
+    text: ""
+  };
+
+  takeText = event => {
+    this.setState({ text: event.target.value });
+  };
+
+  render() {
+    return (
+      <>
+        <input type="text" value={this.state.text} onChange={this.takeText} />
+      </>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Example />, rootElement);
